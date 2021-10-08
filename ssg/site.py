8852f3388bb019f36a855a73a2c90eb648798671
Meghan Pund <meghan.pund@gmail.com>
2021-10-08 14:25:15 -0400

    site.py updates

--- ssg/site.py
+++ ssg/site.py
@@ -3,8 +3,8 @@ from pathlib import Path
 class Site:
     
     def __init__(self, source, dest):
-        self.source = source.Path()
-        self.dest = dest.Path()
+        self.source = Path(source)
+        self.dest = Path(dest)
 
     def create_dir(self, path):
         directory = 'c:/Users/Brazuka/python-static-site-generator/'
