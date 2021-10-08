6665ab36716938b5623698a0879a4a5f5efae765
Meghan Pund <meghan.pund@gmail.com>
2021-10-08 14:34:36 -0400

    finished Site class

--- ssg/site.py
+++ ssg/site.py
@@ -7,4 +7,12 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = 'c:/Users/Brazuka/python-static-site-generator/'
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build():
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
+    
