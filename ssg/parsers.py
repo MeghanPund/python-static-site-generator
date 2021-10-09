1b2280cc75326d41196beae9ee81ab17da49e7ca
Meghan Pund <meghan.pund@gmail.com>
2021-10-09 12:30:56 -0400

    finished Parser class

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,6 +1,9 @@
+import shutil
+
 from os import close, path, read, stat_result, write
 from typing import List
 from pathlib import Path
+
 
 class Parser:
     extensions: List[str] = []
@@ -19,3 +22,12 @@ class Parser:
         full_path = dest / path.with_suffix(ext).name
         with open(full_path, "w") as file:
             file.write(content)
+
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
+
+    class ResourceParser(Parser):
+        extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+        
+        def parse(self, path, source, dest):
+            self.copy(path, source, dest)
